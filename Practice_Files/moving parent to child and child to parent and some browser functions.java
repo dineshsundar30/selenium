6b862a2e9b48989edbf3fe288b7e9097569fae21
Dineshkumar S <113044062+dineshsundar30@users.noreply.github.com>
2024-10-03 17:55:49 +0530

    practice File - 4

---
+++ Practice_Files/moving parent to child and child to parent and some browser functions.java
@@ -0,0 +1,39 @@
+package selenium_test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class seleniumtest3 {
+// moving parent to child and child to parent and some browser functions
+	public static void main(String[] args) {
+	
+		WebDriver driver = new ChromeDriver();
+
+		// Sibling - Child to parent traverse
+
+		//header/div/button[1]/following-sibling::button[1]
+
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+
+		System.out.println(driver.findElement(By.xpath("//header/div/button[1]/following-sibling::button[1]")).getText());  //moving parent to child
+
+		System.out.println(driver.findElement(By.xpath("//header/div/button[1]/parent::div/button[2]")).getText()); // moving child to parent
+		
+		System.out.println("---------------------------------------------------------------------");
+		
+	
+		
+		driver.manage().window().maximize();
+
+		driver.get("http://google.com");
+
+		driver.navigate().to("https://rahulshettyacademy.com");
+
+		driver.navigate().back();
+
+		driver.navigate().forward();
+		
+		driver.close();
+
+	}
+
+}
